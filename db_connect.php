09896d7dcf5a2804104e2dae91d6c4c9cef1d9dd
Hutan Stefan <61848535+hutanstefan@users.noreply.github.com>
2024-04-05 15:05:40 +0300

    db

---
+++ db_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "127.0.0.1:3307";
+$username = "root";
+$password = "";
+
+
+try {
+    $dbConnection = new PDO("mysql:host=$servername;dbname=projectdb", $username, $password);
+    // set the PDO error mode to exception
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully to database";
+} catch (PDOException $e) {
+    echo "Connection failed to database: " . $e->getMessage();
+}
+
