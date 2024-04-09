9c8d093597d8bb655c8c36389dd84bb03c5853c9
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-04-09 15:31:30 +0300

    Update2

---
+++ element-pagin.php
@@ -0,0 +1,25 @@
+<?php
+require_once "NavBar.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Romanina Drug Explorer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar">
+    <div class="navbar__links">
+        <a href="HomePage.php" class="navbar__link">Home</a>
+        <a href="#news" class="navbar__link">Campaigns</a>
+        <a href="#contact" class="navbar__link">Contact Us</a>
+        <a href="#about" class="navbar__link">About</a>
+    </div>
+    <div class="navbar__title">
+        Romanina Drug Explorer
+    </div>
+</nav>
+</body>
+</html>
