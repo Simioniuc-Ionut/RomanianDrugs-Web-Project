0258cd904f0e84f84e5b8b187a900447b9e9adab
Hutan Stefan <61848535+hutanstefan@users.noreply.github.com>
2024-04-09 12:26:36 +0300

    add

--- add_drugs_process.php
+++ add_drugs_process.php
@@ -9,10 +9,14 @@ $new_drug_id = $max_id_row['max_id'] + 1;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($_POST['name']) && !empty($_POST['type']) && !empty($_POST['image']) && !empty($_POST['description'])) {
-        $insert_statement = $dbConnection->prepare("INSERT INTO drugstable (id, name, type, image, description) VALUES (?, ?, ?, ?, ?)");
-        $insert_statement->execute(array($new_drug_id, $_POST['name'], $_POST['type'], $_POST['image'], $_POST['description']));
+        $types = implode(',', $_POST['type']);
 
-        header("Location: add_drugs.php");
+        $insert_statement = $dbConnection->prepare("INSERT INTO drugstable (id, name, type, image, description) VALUES (?, ?, ?, ?, ?)");
+        $insert_statement->execute(array($new_drug_id, $_POST['name'], $types, $_POST['image'], $_POST['description']));
+
+        echo "<p>Medicamentul a fost adăugat cu succes!</p>";
+
+        header("refresh:3;url=add_drugs.php");
         exit();
     } else {
         echo "Toate câmpurile sunt obligatorii.";
