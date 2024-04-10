d3cdc615b0a91c710aad838ad664d9851d93bf53
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-04-10 16:15:26 +0300

    Update

--- element-pagin.php
+++ element-pagin.php
@@ -8,7 +8,12 @@ require_once "NavBar.php";
     <meta charset="UTF-8">
     <title>Romanina Drug Explorer</title>
     <link rel="stylesheet" href="style.css">
+<<<<<<< Updated upstream
     <?php include "NavBar.php"; ?>
+=======
+    <link rel="stylesheet" href="style_element_pagina.css">
+    <?php include 'NavBar.php'; ?>
+>>>>>>> Stashed changes
 </head>
 <body>
 
