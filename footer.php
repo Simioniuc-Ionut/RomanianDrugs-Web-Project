42f2627b37598a0d3787c6973220fc4acf1328e3
Hutan Stefan <61848535+hutanstefan@users.noreply.github.com>
2024-04-15 15:23:28 +0300

    type

--- footer.php
+++ footer.php
@@ -19,28 +19,7 @@
                 <li><a href="#">Collection</a></li>
             </ul>
         </div>
-        <div class="footer-col">
-            <h4>Explore</h4>
-            <ul class="links">
-                <li><a href="#">Free Designs</a></li>
-                <li><a href="#">Latest Designs</a></li>
-                <li><a href="#">Themes</a></li>
-                <li><a href="#">Popular Designs</a></li>
-                <li><a href="#">Art Skills</a></li>
-                <li><a href="#">New Uploads</a></li>
-            </ul>
-        </div>
-        <div class="footer-col">
-            <h4>Legal</h4>
-            <ul class="links">
-                <li><a href="#">Customer Agreement</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">GDPR</a></li>
-                <li><a href="#">Security</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Media Kit</a></li>
-            </ul>
-        </div>
+
         <div class="footer-col">
             <h4>Newsletter</h4>
             <p>
