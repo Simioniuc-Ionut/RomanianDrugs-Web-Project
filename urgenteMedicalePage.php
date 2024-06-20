abe8dafc9e8f6fd21c491e080b32a8658760b813
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-06-20 18:52:12 +0300

    -new pages:

--- urgenteMedicalePage.php
+++ urgenteMedicalePage.php
@@ -34,6 +34,28 @@ global $dbConnection;
         <p class="item-description"><strong>Description:</strong> Datele statistice despre urgențele medicale înregistrate în diferite categorii. Aceste date sunt esențiale pentru a înțelege tendințele și modelele de urgențe medicale și pentru a informa strategiile de prevenire și intervenție.</p>
     </div>
 </div>
+<div class="container_item">
+    <div class="campaign">
+        <h1 class="campaign-title">Campanii de Prevenire a Consumului de Droguri</h1>
+        <p class="campaign-description">Consumul de droguri este o problemă majoră în societatea modernă, afectând milioane de vieți în fiecare an. Pentru a combate această problemă, au fost lansate numeroase campanii de prevenire a consumului de droguri, atât la nivel local, cât și la nivel național și global.</p>
+
+        <h2 class="campaign-section-title">Scopul Campaniilor de Prevenire</h2>
+        <p class="campaign-section-description">Scopul principal al acestor campanii este de a informa și educa publicul despre pericolele și riscurile asociate consumului de droguri. Ele își propun să reducă prevalența consumului de droguri în rândul populației și să promoveze un stil de viață sănătos și lipsit de substanțe toxice.</p>
+
+        <h2 class="campaign-section-title">Inițiative și Proiecte</h2>
+        <p class="campaign-section-description">Campaniile de prevenire a consumului de droguri implică o varietate de inițiative și proiecte, inclusiv:</p>
+        <ul class="campaign-list">
+            <li>Educație și conștientizare în școli și comunități</li>
+            <li>Distribuirea de materiale informative și resurse educaționale</li>
+            <li>Evenimente și întâlniri comunitare pentru promovarea unui stil de viață sănătos</li>
+            <li>Campanii media și sociale pentru sensibilizarea publicului</li>
+            <li>Programare de consiliere și sprijin pentru cei afectați de consumul de droguri</li>
+        </ul>
+
+        <h2 class="campaign-section-title">Implicarea Comunității</h2>
+        <p class="campaign-section-description">Un aspect important al acestor campanii este implicarea comunității. Prin colaborarea între organizațiile guvernamentale și neguvernamentale, grupurile comunitare și membrii societății în general, putem construi un mediu mai sănătos și mai sigur pentru toți.</p>
+    </div>
+</div>
 
 <div class="tab-buttons">
     <button class="tablinks" onclick="openTab(event, 'Graph')">Graph</button>
