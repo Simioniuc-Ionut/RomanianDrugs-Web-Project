a2c70e267ec0fba44dc4e707d97ba6cb84449b14
Hutan Stefan <61848535+hutanstefan@users.noreply.github.com>
2024-06-21 22:17:48 +0300

    contact

--- contact_us.php
+++ contact_us.php
@@ -15,29 +15,29 @@
 </head>
 <body>
 
-<div class="container-form">
+<div class="container-form-contact">
     <form action="sendContactEmail.php" method="post" id="contactForm">
-        <h1 class="form-title">Contact Us</h1>
+        <h1 class="form-title-contact">Contact Us</h1>
 
-        <div class="form-group">
-            <label for="name" class="form-label">Name:</label>
-            <input type="text" id="name" name="name" class="form-input" required>
-            <span class="error" id="nameError"></span>
+        <div class="form-group-contact">
+            <label for="name" class="form-label-contact">Name:</label>
+            <input type="text" id="name" name="name" class="form-input-contact" required>
+            <span class="error-contact" id="nameError"></span>
         </div>
 
-        <div class="form-group">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" id="email" name="email" class="form-input" required>
-            <span class="error" id="emailError"></span>
+        <div class="form-group-contact">
+            <label for="email" class="form-label-contact">Email:</label>
+            <input type="email" id="email" name="email" class="form-input-contact" required>
+            <span class="error-contact" id="emailError"></span>
         </div>
 
-        <div class="form-group">
-            <label for="message" class="form-label">Message:</label>
-            <textarea id="message" name="message" class="form-textarea" required></textarea>
-            <span class="error" id="messageError"></span>
+        <div class="form-group-contact">
+            <label for="message" class="form-label-contact">Message:</label>
+            <textarea id="message" name="message" class="form-textarea-contact" required></textarea>
+            <span class="error-contact" id="messageError"></span>
         </div>
 
-        <button type="submit" class="form-button">Submit</button>
+        <button type="submit" class="form-button-contact">Submit</button>
     </form>
 </div>
 
