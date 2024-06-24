d74a038420f19853b85044e3d98ca95fc65b355c
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-06-24 19:05:45 +0300

    -data from db

---
+++ exportDbData/admins.sql
@@ -0,0 +1,5 @@
+INSERT INTO projectdb.admins (id, username, password_hash) VALUES (2, '123', '$2y$10$wjKJqP1Kulirn3zf7idwXejkAxtbKl7dRg9Z1MjiYa9WlMZzB4sty');
+INSERT INTO projectdb.admins (id, username, password_hash) VALUES (3, '1234', '$2y$10$pJ86krBexxePyxGPQI8WYONOamjmQu7/WjgpvOeGp9DsiUSzmT98K');
+INSERT INTO projectdb.admins (id, username, password_hash) VALUES (5, '12345', '$2y$10$78sgnmRcxr21MCEKNV1Ie.1rdlRJaoXctKJJ9NE/vwv7wqTk7Z7yG');
+INSERT INTO projectdb.admins (id, username, password_hash) VALUES (6, '123456', '$2y$10$A1Q8V8ytxyB//DwO7OMue.KHuXnpMVv6FHPMx.1GPI4roDfM7WGc2');
+INSERT INTO projectdb.admins (id, username, password_hash) VALUES (7, 'ana', '$2y$10$0b7T/iulWNVF4DEDlIrDHeVq2PULu0p9fJvENMlX2uiej9bp1Amxq');
